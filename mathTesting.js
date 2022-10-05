02c587eb7292c610e399ff91a0d5341b06b50c15
DamesNicholas-FS <nrdames@student.fullsail.edu>
2022-10-05 11:18:41 -0400

    fixing issues
    fix this issues, i am using classes to call on. this i feel was the best method. i have left the old code commented out  to be able to look back to see what i was doing wrong

--- mathTesting.js
+++ mathTesting.js
@@ -1,25 +1,49 @@
-const add = (x, y) => {
-    return (x + y)
-}
+// const add = (x, y) => {
+//     return (x + y)
+// }
 
-const subtract = (x, y) => {
-    return (x + y)
-}
+// const subtract = (x, y) => {
+//     return (x - y)
+// }
 
-const divide = (x, y) => {
-    return (x + y)
-}
+// const divide = (x, y) => {
+//     return (x / y)
+// }
 
-const multiply = (x, y) => {
-    return x + y
-}
+// const multiply = (x, y) => {
+//     return (x * y)
+// }
 
-const sqRoot = (a,b) => {
-    return (Math.sqrt((a * a) + (b * b)));
-}
+// const sqRoot = (a,b) => {
+//     return (Math.sqrt((a * a) + (b * b)));
+// }
 
-const maxNumber = (a,b,c) =>{
-    return (Math.max(a,b,c))
-}
+// const maxNumber = (a,b,c) =>{
+//     return (Math.max(a,b,c))
+// }
 
-module.exports = add, subtract, divide, multiply, sqRoot, maxNumber
+// module.exports = add, subtract, divide, multiply, sqRoot, maxNumber
+
+const mathTesting = {
+    additional:function(x,y){
+        return x + y
+    },
+    subtraction: function(x,y){
+        return x - y
+    },
+    multiplication: function(x,y){
+        return x * y
+    },
+    division: function(x,y){
+        return x / y
+    },
+    sqrt: function(x,y){
+        return (Math.sqrt((x * x) + (y * y)));
+    },
+    max: function(x,y,z){
+        return (Math.max(x,y,z))
+    },
+    
+    
+}
+module.exports = mathTesting
--- mathTesting.test.js
+++ mathTesting.test.js
@@ -1,32 +1,57 @@
-const add = require("./mathTesting");
-const subtract = require("./mathTesting");
-const divide = require("./mathTesting");
-const multiply = require("./mathTesting");
-const sqRoot = require("./mathTesting");
-const maxNumber = require("./mathTesting");
 
-describe('Testing Math Skills', () =>{
-    test("96+55=151", ()=>{
-       expect(add(96,55)).toEqual(151)
-    }),
+// const add = require("./mathTesting");
+// const subtract = require("./mathTesting");
+// const divide = require("./mathTesting");
+// const multiply = require("./mathTesting");
+// const sqRoot = require("./mathTesting");
+// const maxNumber = require("./mathTesting");
 
-    test("33 - 27 = 6", ()=>{
-        expect(subtract(33,27)).toEqual(6)
-    }),
+// describe('Testing Math Skills', () =>{
+//     test("96+55=151", ()=>{
+//        expect(add(96,55)).toEqual(151)
+//     })
 
-    test("100 ÷ 2 = 6", ()=>{
-        expect(divide(100,2)).toEqual(50)
-    }),
+//     test("33 - 27 = 6", ()=>{
+//         expect(subtract(33,27)).toEqual(6)
+//     })
 
-    test("13 × 2 = 26", ()=>{
-        expect(multiply(13,2)).toEqual(26)
-    }),
+//     test("100 ÷ 2 = 6", ()=>{
+//         expect(divide(100,2)).toEqual(50)
+//     })
 
-    test("(3×3) + (4×4) = 5", ()=>{
-        expect(sqRoot(3,4)).toEqual(5)
-    }),
+//     test("13 × 2 = 26", ()=>{
+//         expect(multiply(13,2)).toEqual(26)
+//     })
 
-    test("Max Number of 1,3,2", ()=>{
-        expect(maxNumber(1,3,2)).toEqual(3)
+//     test("(3×3) + (4×4) = 5", ()=>{
+//         expect(sqRoot(3,4)).toEqual(5)
+//     })
+
+//     test("Max Number of 1,3,2", ()=>{
+//         expect(maxNumber(1,3,2)).toEqual(3)
+//     })
+// })
+
+const mathTesting =require ("./mathTesting")
+
+describe('Testing Math Skills', () => {
+    test('5 + 23 = 28', () => {
+        expect(mathTesting.additional(5,23)).toEqual(28)
     })
-})
+    test('28 - 5 = 23', () => {
+        expect(mathTesting.subtraction(28,5)).toEqual(23)
+    })
+    test('12 x 12 = 144', () => {
+        expect(mathTesting.multiplication(12,12)).toEqual(144)
+    })
+    test('100 / 5 = 20', () => {
+        expect(mathTesting.division(100,5)).toEqual(20)
+    })
+    test('sqrt of 9 & 16', () => {
+        expect(mathTesting.sqrt(3,4)).toEqual(5)
+    })
+    test('Max number of 99,23,34 ', () => {
+        expect(mathTesting.max(99,23,34)).toBe(99)
+    })
+    
+});
